8fc925f3803b7961bbf6e5e03000b6d8969d5e8c
chrismo80 <krizm0@freenet.de>
2024-11-13 13:49:25 +0100

    Prettify base test class

--- UnitTests/ManagerTests.cs
+++ UnitTests/ManagerTests.cs
@@ -2,14 +2,15 @@ using CompanyName.Core;
 
 namespace UnitTests;
 
-public abstract class ManagerTests<T> where T : Manager
+public abstract class ManagerTests<T>
+	where T : Manager, new()
 {
 	protected T Sut = null!;
 
 	[TestInitialize]
 	public void Initialize()
 	{
-		Sut = Activator.CreateInstance<T>();
+		Sut = new T();
 
 		InitialAsserts();
 	}
