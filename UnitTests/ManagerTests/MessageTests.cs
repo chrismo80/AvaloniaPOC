0f6d7d5cc40c6c08721afb213081951a657d66ae
chrismo80 <krizm0@freenet.de>
2024-11-13 17:20:17 +0100

    Add ConfirmMessage unit test

--- UnitTests/ManagerTests/MessageTests.cs
+++ UnitTests/ManagerTests/MessageTests.cs
@@ -39,6 +39,26 @@ public class MessageTests : ManagerTests<MessageManager>
 	}
 
 	[TestMethod]
+	public void ConfirmMessage_AfterArchiveMessage_MessageMoved()
+	{
+		Sut.CreateMessage("Message 1", MessageType.Information);
+		Sut.CreateMessage("Message 2", MessageType.Information);
+
+		Assert.AreEqual(2, Sut.ActiveMessages.Count);
+		Assert.AreEqual(0, Sut.ArchivedMessages.Count);
+
+		var firstMessage = Sut.ActiveMessages.First();
+
+		Sut.Archive(firstMessage);
+		Sut.Confirm(firstMessage);
+
+		Assert.AreEqual(1, Sut.ActiveMessages.Count);
+		Assert.AreEqual(1, Sut.ArchivedMessages.Count);
+
+		Assert.AreEqual(firstMessage, Sut.ArchivedMessages.Last());
+	}
+
+	[TestMethod]
 	public void CreateMessage_WithExtension_Success()
 	{
 		this.CreateMessage("Message", MessageType.Information);
