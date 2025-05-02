13e8c46dd85eedf3a7bdb7ab3627a430d1819124
chrismo80 <krizm0@freenet.de>
2025-05-02 22:47:47 +0200

    Cleanup usage of test extension

--- UnitTests/ServiceTests/AuthTests.cs
+++ UnitTests/ServiceTests/AuthTests.cs
@@ -5,80 +5,64 @@ namespace UnitTests.ServiceTests;
 [TestClass]
 public class AuthTests : ServiceTests<AuthManager>
 {
-    private const string USER_NAME = "user";
-    private const string CORRECT_PASSWORD = "pass";
-    private const string WRONG_PASSWORD = "password";
+	private const string USER_NAME = "user";
+	private const string CORRECT_PASSWORD = "pass";
+	private const string WRONG_PASSWORD = "password";
 
-    [TestMethod]
-    public void Login_NoUserNoPassword_NotLoggedIn()
-    {
-        var success = Sut.Login("", "");
+	[TestMethod]
+	public void Login_NoUserNoPassword_NotLoggedIn()
+	{
+		Sut.Login("", "").Is(false);
+		Sut.User.Is("");
+	}
 
-        success.Is(false);
-        Sut.User.Is("");
-    }
+	[TestMethod]
+	public void Login_NoUserButPassword_NotLoggedIn()
+	{
+		Sut.Login("", CORRECT_PASSWORD).Is(false);
+		Sut.User.Is("");
+	}
 
-    [TestMethod]
-    public void Login_NoUserButPassword_NotLoggedIn()
-    {
-        var success = Sut.Login("", CORRECT_PASSWORD);
+	[TestMethod]
+	public void Login_UserButNoPassword_NotLoggedIn()
+	{
+		Sut.Login(USER_NAME, "").Is(false);
+		Sut.User.Is("");
+	}
 
-        success.Is(false);
-        Sut.User.Is("");
-    }
+	[TestMethod]
+	public void Login_WrongPassword_NotLoggedIn()
+	{
+		Sut.Login(USER_NAME, WRONG_PASSWORD).Is(false);
+		Sut.User.Is("");
+	}
 
-    [TestMethod]
-    public void Login_UserButNoPassword_NotLoggedIn()
-    {
-        var success = Sut.Login(USER_NAME, "");
+	[TestMethod]
+	public void Login_CorrectPassword_LoggedIn()
+	{
+		Sut.Login(USER_NAME, CORRECT_PASSWORD).Is(true);
+		Sut.User.Is(USER_NAME);
+	}
 
-        success.Is(false);
-        Sut.User.Is("");
-    }
+	[TestMethod]
+	public void Logout_AfterLogin_LoggedOut()
+	{
+		Sut.Login(USER_NAME, CORRECT_PASSWORD).Is(true);
+		Sut.User.Is(USER_NAME);
 
-    [TestMethod]
-    public void Login_WrongPassword_NotLoggedIn()
-    {
-        var success = Sut.Login(USER_NAME, WRONG_PASSWORD);
+		Sut.Logout().Is(true);
+		Sut.User.Is("");
+	}
 
-        success.Is(false);
-        Sut.User.Is("");
-    }
+	[TestMethod]
+	public void Logout_WithoutLogin_NoException()
+	{
+		Sut.Logout().Is(false);
+		Sut.User.Is("");
+	}
 
-    [TestMethod]
-    public void Login_CorrectPassword_LoggedIn()
-    {
-        var success = Sut.Login(USER_NAME, CORRECT_PASSWORD);
-
-        success.Is(true);
-        Sut.User.Is(USER_NAME);
-    }
-
-    [TestMethod]
-    public void Logout_AfterLogin_LoggedOut()
-    {
-        var success = Sut.Login(USER_NAME, CORRECT_PASSWORD);
-
-        success.Is(true);
-        Sut.User.Is(USER_NAME);
-
-        success = Sut.Logout();
-
-        success.Is(true);
-        Sut.User.Is("");
-    }
-
-    [TestMethod]
-    public void Logout_WithoutLogin_NoException()
-    {
-        var success = Sut.Logout();
-
-        success.Is(false);
-        Sut.User.Is("");
-    }
-
-    protected override void Pre()
-    {
-        Sut.User.Is("");
-    }
+	protected override void Pre()
+	{
+		Sut.User.Is("");
+	}
 }
