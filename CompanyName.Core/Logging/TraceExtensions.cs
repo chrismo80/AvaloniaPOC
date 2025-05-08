8301ec5d9ed8f1a613058da94c8b10bec962cc71
cmoldenhauer <cm@imstec.de>
2025-05-08 10:40:27 +0200

    Improve TraceWatch

--- CompanyName.Core/Logging/TraceExtensions.cs
+++ CompanyName.Core/Logging/TraceExtensions.cs
@@ -19,9 +19,18 @@ public static class TraceExtensions
 	public static void Trace(this object me, Exception ex) =>
 		me.Trace(ex.ToString(), LogLevel.Error);
 
+	public static string ToValidFileName(this string fileName)
+	{
+		foreach (char c in Path.GetInvalidFileNameChars())
+			fileName = fileName.Replace(c, '-');
+
+		return fileName;
+	}
+
 	private static string FormatMessage(this object me, string text) =>
 		GetTypeText(me.GetType().FullName ?? "") + ":\t" + text;
 
 	private static string GetTypeText(string type = "") =>
 		type.Length < TYPE_LENGTH ? type.PadLeft(TYPE_LENGTH) : type[^TYPE_LENGTH..];
+
 }
--- CompanyName.Core/Logging/TraceWatch.cs
+++ CompanyName.Core/Logging/TraceWatch.cs
@@ -1,24 +1,39 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace CompanyName.Core.Logging;
 
-public class TraceWatch : IDisposable
+public class TraceWatch : Stopwatch, IDisposable
 {
-	readonly System.Diagnostics.Stopwatch _stopwatch;
-
-	readonly string _message;
 	readonly object? _caller;
+
+	public string Message { get; set; }
 
 	public TraceWatch(object? caller = null, string? message = null, [CallerMemberName] string method = "")
 	{
-		_message = message ?? $"Method '{method}'";
-		_caller = caller;
+		_caller = caller ?? this;
 
-		(_caller ?? this).Trace($"{_message} begin");
+		Message = message ?? $"-------------- Method '{method}'";
 
-		_stopwatch = System.Diagnostics.Stopwatch.StartNew();
+		//_caller.Trace($"{Message} started");
+
+		Start();
 	}
 
-	public void Dispose() =>
-		(_caller ?? this).Trace($"{_message} end (Duration={_stopwatch.Elapsed.TotalSeconds:F3}s)");
+	public void Dispose()
+	{
+		Stop();
+
+		//Export(Message, $"{Elapsed.TotalSeconds:F3}");
+
+		_caller!.Trace($"{Message} end (Duration={Elapsed.TotalSeconds:F3}s)");
+	}
+
+	private void Export(string fileName, string data)
+	{
+		var file = Path.Combine("Durations", _caller.ToString(), fileName.ToValidFileName() + ".txt");
+
+		Directory.CreateDirectory(Path.GetDirectoryName(file)!);
+		File.AppendAllText(file, data + Environment.NewLine);
+	}
 }
