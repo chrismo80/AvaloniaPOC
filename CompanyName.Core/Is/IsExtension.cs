fb973a31e489a5b306a1411721f3f64c98ac49e0
chrismo80 <krizm0@freenet.de>
2025-05-18 22:45:23 +0200

    Cleanup

--- CompanyName.Core/Is/IsExtension.cs
+++ CompanyName.Core/Is/IsExtension.cs
@@ -14,14 +14,15 @@ public static class IsExtension
 		!AreEqual(actual, expected) ? true : throw new IsException(actual, expected);
 
 	private static bool AreEqual(object actual, params object[]? expected) =>
-		actual.VerifyEqualityTo(expected?.Unwrap());
+		actual.ShouldBe(expected?.Unwrap());
 
-	private static bool VerifyEqualityTo(this object actual, object[]? expected) => expected?.Length switch
-	{
-		null => actual.IsEqualTo(null),
-		1 => actual.IsEqualTo(expected[0]),
-		_ => actual.ToArray().Are(expected)
-	};
+	private static bool ShouldBe(this object actual, object[]? expected) =>
+		expected?.Length switch
+		{
+			null => actual.IsEqualTo(null),
+			1 => actual.IsEqualTo(expected[0]),
+			_ => actual.ToArray().Are(expected)
+		};
 
 	private static object[] Unwrap(this object[] array) =>
 		array is [IEnumerable first and not string] ? first.ToArray() : array;
@@ -34,4 +35,27 @@ public static class IsExtension
 
 	private static bool IsEqualTo<T>(this T? actual, T? expected) =>
 		EqualityComparer<T>.Default.Equals(actual, expected);
+}
+
+public class IsException(object? actual, object? expected)
+	: Exception(actual.Format() + " is " + expected.Format());
+
+public class IsNotException(object? actual, object? expected)
+	: Exception(actual.Format() + " is not " + expected.Format());
+
+internal static class ExceptionFormatter
+{
+	internal static string Format(this object? value) =>
+		value.Value() + value.Type();
+
+	private static string Value(this object? value) => value switch
+	{
+		null => "<NULL>",
+		string => $"\"{value}\"",
+		IEnumerable enumerable => $"[{string.Join("|", enumerable.Cast<object>())}]",
+		_ => $"{value}"
+	};
+
+	private static string Type(this object? value) =>
+		value is null or System.Type ? "" : $" ({value.GetType()})";
 }
