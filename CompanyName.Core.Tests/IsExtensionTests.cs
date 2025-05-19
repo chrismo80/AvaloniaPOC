0f60e1e3a41f37a90e89a5a5b37cace3501850d2
cmoldenhauer <cm@imstec.de>
2025-05-19 08:32:06 +0200

    Remove Is exceptions again

--- CompanyName.Core.Tests/IsExtensionTests.cs
+++ CompanyName.Core.Tests/IsExtensionTests.cs
@@ -5,108 +5,97 @@ namespace CompanyName.Core.Tests;
 [TestClass]
 public class IsExtensionTests
 {
-	[TestMethod]
-	[DataRow(null, null)]
-	[DataRow(false, false)]
-	[DataRow(true, true)]
-	[DataRow(1, 1)]
-	[DataRow(2.2, 2.2)]
-	[DataRow(3f, 3f)]
-	[DataRow("4", "4")]
-	public void Is_Actual_Equals_Expected(object actual, object expected) =>
-		actual.Is(expected);
+    [TestMethod]
+    [DataRow(null, null)]
+    [DataRow(false, false)]
+    [DataRow(true, true)]
+    [DataRow(1, 1)]
+    [DataRow(2.2, 2.2)]
+    [DataRow(3f, 3f)]
+    [DataRow("4", "4")]
+    public void Is_Actual_Equals_Expected(object actual, object expected) =>
+        actual.Is(expected);
 
-	[ExpectedException(typeof(IsException))]
-	[TestMethod]
-	[DataRow(null, null)]
-	[DataRow(false, false)]
-	[DataRow(true, true)]
-	[DataRow(1, 1)]
-	[DataRow(2.2, 2.2)]
-	[DataRow(3f, 3f)]
-	[DataRow("4", "4")]
-	public void IsNot_Actual_Equals_Expected(object actual, object expected) =>
-		actual.IsNot(expected);
+    [TestMethod]
+    [DataRow(null, null)]
+    [DataRow(false, false)]
+    [DataRow(true, true)]
+    [DataRow(1, 1)]
+    [DataRow(2.2, 2.2)]
+    [DataRow(3f, 3f)]
+    [DataRow("4", "4")]
+    public void IsNot_Actual_Equals_Expected(object actual, object expected) =>
+        Assert.ThrowsException<Exception>(() => actual.IsNot(expected));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	[DataRow(null, true)]
-	[DataRow(null, false)]
-	[DataRow(true, false)]
-	[DataRow(5, null)]
-	[DataRow(6, 6d)]
-	[DataRow(7, true)]
-	[DataRow(false, 7d)]
-	[DataRow(8d, 8f)]
-	[DataRow(99, "99")]
-	[DataRow("ABC", false)]
-	[DataRow("ABC", null)]
-	[DataRow("ABC", "ABD")]
-	public void Actual_Not_Equals_Expected(object actual, object expected)
-	{
-		actual.Is(expected);
-		expected.Is(actual);
-	}
+    [TestMethod]
+    [DataRow(null, true)]
+    [DataRow(null, false)]
+    [DataRow(true, false)]
+    [DataRow(5, null)]
+    [DataRow(6, 6d)]
+    [DataRow(7, true)]
+    [DataRow(false, 7d)]
+    [DataRow(8d, 8f)]
+    [DataRow(99, "99")]
+    [DataRow("ABC", false)]
+    [DataRow("ABC", null)]
+    [DataRow("ABC", "ABD")]
+    public void Actual_Not_Equals_Expected(object actual, object expected) =>
+        Assert.ThrowsException<Exception>(() => actual.Is(expected));
 
-	[TestMethod]
-	public void ListValues_Equal_Expected() =>
-		VerifyEquality(new List<int> { 1, 2, 3, 4 });
+    [TestMethod]
+    public void ListValues_Equal_Expected() =>
+        VerifyEquality(new List<int> { 1, 2, 3, 4 });
 
-	[TestMethod]
-	public void ArrayValues_Equal_Expected() =>
-		VerifyEquality(new int[] { 1, 2, 3, 4 });
+    [TestMethod]
+    public void ArrayValues_Equal_Expected() =>
+        VerifyEquality(new int[] { 1, 2, 3, 4 });
 
-	private void VerifyEquality(IEnumerable<int> values)
-	{
-		values.Is(new int[] { 1, 2, 3, 4 });
-		values.Is(new List<int> { 1, 2, 3, 4 });
-		values.Is(1, 2, 3, 4);
-		values.Where(i => i % 2 == 0).Is(2, 4);
-	}
+    [TestMethod]
+    public void ValuesWithNull_Equal_Expected() =>
+        new int?[] { 1, 2, null, 4 }.Is(1, 2, null, 4);
 
-	[TestMethod]
-	public void ValuesWithNull_Equal_Expected() =>
-		new int?[] { 1, 2, null, 4 }.Is(1, 2, null, 4);
+    [TestMethod]
+    public void List_Not_Equal_List() =>
+        Assert.ThrowsException<Exception>(() => new List<int> { 1, 2, 3, 5 }.Is(new List<int> { 1, 2, 3, 4 }));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void List_Not_Equal_List() =>
-		new List<int> { 1, 2, 3, 5 }.Is(new List<int> { 1, 2, 3, 4 });
+    [TestMethod]
+    public void Array_Not_Equal_Params() =>
+        Assert.ThrowsException<Exception>(() => new List<int> { 1, 2, 3, 5 }.Is(new List<int> { 1, 2, 3, 4 }));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void Array_Not_Equal_Params() =>
-		new int[] { 1, 2, 3, 5 }.Is(1, 2, 3, 4);
+    [TestMethod]
+    public void IEnumerable_Not_Equal_Params_TooShort() =>
+        Assert.ThrowsException<Exception>(() => new List<int> { 1, 2, 3, 5 }.Where(i => i % 2 == 0).Is(2, 4));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void IEnumerable_Not_Equal_Params_TooShort() =>
-		new List<int> { 1, 2, 3, 5 }.Where(i => i % 2 == 0).Is(2, 4);
+    [TestMethod]
+    public void IEnumerable_Not_Equal_Params_TooLong() =>
+        Assert.ThrowsException<Exception>(() => new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 2 == 0).Is(2, 4));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void IEnumerable_Not_Equal_Params_TooLong() =>
-		new List<int> { 1, 2, 3, 4, 5, 6 }.Where(i => i % 2 == 0).Is(2, 4);
+    [TestMethod]
+    public void JaggedArrays_Equals_Expected() =>
+        new object[] { new[] { 1, 2 }, 3 }.Is(new object[] { new[] { 1, 2 }, 3 });
 
-	[TestMethod]
-	public void JaggedArrays_Equals_Expected() =>
-		new object[] { new[] { 1, 2 }, 3 }.Is(new object[] { new[] { 1, 2 }, 3 });
+    [TestMethod]
+    public void DifferentDepth_EqualsThough_Expected() =>
+        new List<object> { 1, 2 }.Is(new List<object> { 1, new List<object> { 2 } });
 
-	[TestMethod]
-	public void DifferentDepth_EqualsThough_Expected() =>
-		new List<object> { 1, 2 }.Is(new List<object> { 1, new List<object> { 2 } });
+    [TestMethod]
+    public void Value_NotEquals_List() =>
+        Assert.ThrowsException<Exception>(() => 5.Is(new List<int> { 1, 2 }));
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void Value_NotEquals_List() =>
-		5.Is(new List<int> { 1, 2 });
+    [TestMethod]
+    public void Value_Is_Type() =>
+        new List<int>().Is<IReadOnlyList<int>>();
 
-	[TestMethod]
-	public void Value_Is_Type() =>
-		new List<int>().Is<IReadOnlyList<int>>();
+    [TestMethod]
+    public void Value_IsNot_Type() =>
+        Assert.ThrowsException<Exception>(() => new List<int>().Is<IReadOnlyList<double>>());
 
-	[ExpectedException(typeof(IsNotException))]
-	[TestMethod]
-	public void Value_IsNot_Type() =>
-		new List<int>().Is<IReadOnlyList<double>>();
+    private static void VerifyEquality(IEnumerable<int> values)
+    {
+        values.Is(new int[] { 1, 2, 3, 4 });
+        values.Is(new List<int> { 1, 2, 3, 4 });
+        values.Is(1, 2, 3, 4);
+        values.Where(i => i % 2 == 0).Is(2, 4);
+    }
 }
--- CompanyName.Core/Is/IsExtension.cs
+++ CompanyName.Core/Is/IsExtension.cs
@@ -4,58 +4,53 @@ namespace CompanyName.Core.Is;
 
 public static class IsExtension
 {
-	public static T Is<T>(this object actual) =>
-		actual is T cast ? cast : throw new IsNotException(actual, typeof(T));
+    public static T Is<T>(this object actual) =>
+        actual is T cast ? cast : throw new Exception(actual.Actually("is not", typeof(T)));
 
-	public static bool Is(this object actual, params object[]? expected) =>
-		AreEqual(actual, expected) ? true : throw new IsNotException(actual, expected);
+    public static bool Is(this object actual, params object[]? expected) =>
+        AreEqual(actual, expected) ? true : throw new Exception(actual.Actually("is not", expected));
 
-	public static bool IsNot(this object actual, params object[]? expected) =>
-		!AreEqual(actual, expected) ? true : throw new IsException(actual, expected);
+    public static bool IsNot(this object actual, params object[]? expected) =>
+        !AreEqual(actual, expected) ? true : throw new Exception(actual.Actually("is", expected));
 
-	private static bool AreEqual(object actual, params object[]? expected) =>
-		actual.ShouldBe(expected?.Unwrap());
+    private static bool AreEqual(object actual, params object[]? expected) =>
+        actual.ShouldBe(expected?.Unwrap());
 
-	private static bool ShouldBe(this object actual, object[]? expected) =>
-		expected?.Length switch
-		{
-			null => actual.IsEqualTo(null),
-			1 => actual.IsEqualTo(expected[0]),
-			_ => actual.ToArray().Are(expected)
-		};
+    private static bool ShouldBe(this object actual, object[]? expected) =>
+        expected?.Length switch
+        {
+            null => actual.IsEqualTo(null),
+            1 => actual.IsEqualTo(expected[0]),
+            _ => actual.ToArray().Are(expected)
+        };
 
-	private static object[] Unwrap(this object[] array) =>
-		array is [IEnumerable first and not string] ? first.ToArray() : array;
+    private static object[] Unwrap(this object[] array) =>
+        array is [IEnumerable first and not string] ? first.ToArray() : array;
 
-	private static object[] ToArray(this object value) =>
-		Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
+    private static object[] ToArray(this object value) =>
+        Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
 
-	private static bool Are(this object[] values, object[] expected) =>
-		values.Length == expected.Length && expected.Length.For().All(i => AreEqual(values[i], expected[i]));
+    private static bool Are(this object[] values, object[] expected) =>
+        values.Length == expected.Length && expected.Length.For().All(i => AreEqual(values[i], expected[i]));
 
-	private static bool IsEqualTo<T>(this T? actual, T? expected) =>
-		EqualityComparer<T>.Default.Equals(actual, expected);
-}
+    private static bool IsEqualTo<T>(this T? actual, T? expected) =>
+        EqualityComparer<T>.Default.Equals(actual, expected) ? true : throw new Exception(actual.Actually("is not", expected));
 
-public class IsException(object? actual, object? expected)
-	: Exception(actual.Format() + " is " + expected.Format());
+    private static string Actually(this object? actual, string actually, object? expected) =>
+        actual.Format() + " actually " + actually + " " + expected.Format();
 
-public class IsNotException(object? actual, object? expected)
-	: Exception(actual.Format() + " is not " + expected.Format());
+    private static string Format(this object? value) =>
+        value.FormatValue() + value.FormatType();
 
-internal static class ExceptionFormatter
-{
-	internal static string Format(this object? value) =>
-		value.Value() + value.Type();
+    private static string FormatValue(this object? value) =>
+        value switch
+        {
+            null => "<NULL>",
+            string => $"\"{value}\"",
+            IEnumerable enumerable => $"[{string.Join("|", enumerable.Cast<object>())}]",
+            _ => $"{value}"
+        };
 
-	private static string Value(this object? value) => value switch
-	{
-		null => "<NULL>",
-		string => $"\"{value}\"",
-		IEnumerable enumerable => $"[{string.Join("|", enumerable.Cast<object>())}]",
-		_ => $"{value}"
-	};
-
-	private static string Type(this object? value) =>
-		value is null or System.Type ? "" : $" ({value.GetType()})";
+    private static string FormatType(this object? value) =>
+        value is null or Type ? "" : $" ({value.GetType()})";
 }
