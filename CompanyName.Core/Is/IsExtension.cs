a6af8d246e6700698bd8d23f593482a64120c819
chrismo80 <krizm0@freenet.de>
2025-05-19 19:25:45 +0200

    auto format

--- CompanyName.Core/Is/IsExtension.cs
+++ CompanyName.Core/Is/IsExtension.cs
@@ -5,60 +5,60 @@ using System.Collections;
 
 public static class IsExtension
 {
-    public static T Is<T>(this object actual) =>
-        actual is T cast ? cast : throw new Exception(actual.Actually("is not", typeof(T)));
+	public static T Is<T>(this object actual) =>
+		actual is T cast ? cast : throw new Exception(actual.Actually("is not", typeof(T)));
 
-    public static bool Is(this object actual, params object[]? expected) =>
-        actual.ShouldBe(expected?.Unwrap()) ? true : throw new Exception(actual.Actually("is not", expected));
+	public static bool Is(this object actual, params object[]? expected) =>
+		actual.ShouldBe(expected?.Unwrap()) ? true : throw new Exception(actual.Actually("is not", expected));
 
-    private static bool ShouldBe(this object actual, object[]? expected) =>
-        expected?.Length switch
-        {
-            null => actual.IsEqualTo(null),
-            1 => actual.IsEqualTo(expected[0]),
-            _ => actual.ToArray().Are(expected)
-        };
+	private static bool ShouldBe(this object actual, object[]? expected) =>
+		expected?.Length switch
+		{
+			null => actual.IsEqualTo(null),
+			1 => actual.IsEqualTo(expected[0]),
+			_ => actual.ToArray().Are(expected)
+		};
 
-    private static object[] Unwrap(this object[] array) =>
-        array is [IEnumerable first and not string] ? first.ToArray() : array;
+	private static object[] Unwrap(this object[] array) =>
+		array is [IEnumerable first and not string] ? first.ToArray() : array;
 
-    private static object[] ToArray(this object value) =>
-        Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
+	private static object[] ToArray(this object value) =>
+		Enumerable.ToArray(value.Is<IEnumerable>().Cast<object>());
 
-    private static bool Are(this object[] values, object[] expected) =>
-        values.Length == expected.Length && expected.Length.For().All(i => values[i].Is(expected[i]));
+	private static bool Are(this object[] values, object[] expected) =>
+		values.Length == expected.Length && expected.Length.For().All(i => values[i].Is(expected[i]));
 
-    private static bool IsEqualTo<T>(this T? actual, T? expected) =>
-        EqualityComparer<T>.Default.Equals(actual, expected) || actual.IsCloseTo(expected) ? true
-        : throw new Exception(actual.Actually("is not", expected));
+	private static bool IsEqualTo<T>(this T? actual, T? expected) =>
+		EqualityComparer<T>.Default.Equals(actual, expected) || actual.IsCloseTo(expected) ? true
+			: throw new Exception(actual.Actually("is not", expected));
 
-    private static bool IsCloseTo<T>(this T? actual, T? expected) =>
-        (actual, expected) switch
-        {
-            (float a, float e) => a.IsInTolerance(e, 1e-6f),
-            (double a, double e) => a.IsInTolerance(e, 1e-6),
-            _ => false,
-        };
+	private static bool IsCloseTo<T>(this T? actual, T? expected) =>
+		(actual, expected) switch
+		{
+			(float a, float e) => a.IsInTolerance(e, 1e-6f),
+			(double a, double e) => a.IsInTolerance(e, 1e-6),
+			_ => false
+		};
 
-    private static bool IsInTolerance<T>(this T actual, T expected, T tolerance) where T : IFloatingPoint<T> =>
-        T.Abs(actual - expected) <= tolerance * T.Max(T.One, T.Abs(expected)) ? true
-        : throw new Exception(actual.Actually("is not close to", expected));
+	private static bool IsInTolerance<T>(this T actual, T expected, T tolerance) where T : IFloatingPoint<T> =>
+		T.Abs(actual - expected) <= tolerance * T.Max(T.One, T.Abs(expected)) ? true
+			: throw new Exception(actual.Actually("is not close to", expected));
 
-    private static string Actually(this object? actual, string equality, object? expected) =>
-        actual.Format() + " actually " + equality + " " + expected.Format();
+	private static string Actually(this object? actual, string equality, object? expected) =>
+		actual.Format() + " actually " + equality + " " + expected.Format();
 
-    private static string Format(this object? value) =>
-        value.FormatValue() + value.FormatType();
+	private static string Format(this object? value) =>
+		value.FormatValue() + value.FormatType();
 
-    private static string FormatValue(this object? value) =>
-        value switch
-        {
-            null => "<NULL>",
-            string => $"\"{value}\"",
-            IEnumerable enumerable => $"[{string.Join("|", enumerable.Cast<object>())}]",
-            _ => $"{value}"
-        };
+	private static string FormatValue(this object? value) =>
+		value switch
+		{
+			null => "<NULL>",
+			string => $"\"{value}\"",
+			IEnumerable enumerable => $"[{string.Join("|", enumerable.Cast<object>())}]",
+			_ => $"{value}"
+		};
 
-    private static string FormatType(this object? value) =>
-        value is null or Type ? "" : $" ({value.GetType()})";
+	private static string FormatType(this object? value) =>
+		value is null or Type ? "" : $" ({value.GetType()})";
 }
